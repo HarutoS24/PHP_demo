6fec1c321a7f86f73cf7b4caa5b78d3411f0977a
HarutoS24 <harutosato2004@gmail.com>
2025-04-17 09:20:39 +0900

    profile表示項目の拡充

--- kintai/src/views/profile.vue
+++ kintai/src/views/profile.vue
@@ -11,23 +11,56 @@
         <el-header>
           HARUTO SATO
         </el-header>
-        <el-main style="font">
-          今まで趣味の範囲でプログラムを書くことが多かったのですが、最近は企業で仕事としてプログラミングをしたりしています！
+        <el-main style="font-size: 18px">
+          趣味・興味があることが言語、プログラミング、車の改造等々しょっちゅう移り変わります！<br>
+          いろんなことが少しだけできる器用貧乏なところがありますが、伸ばしていければと思ってます！
         </el-main>
       </el-container>
     </el-container>
   </el-card>
   <el-card>
-    経歴
+    <el-header>経歴</el-header>
+    <el-table :data="history_data">
+      <el-table-column prop="name" label=""></el-table-column>
+      <el-table-column prop="sentence" label="" />
+    </el-table>
   </el-card>
   <el-card>
-    資格
+    <el-header>資格</el-header>
+    <el-table :data="skills_data">
+      <el-table-column prop="name" label=""></el-table-column>
+      <el-table-column prop="sentence" label="" />
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-  name: 'profile'
+  name: 'profile',
+  data() {
+    return {
+      history_data: [
+        {
+          'name': '滋賀県立彦根東高等学校',
+          'sentence': '2019/04-2022/03'
+        },
+        {
+          'name': '大阪公立大学',
+          'sentence': '2022/04-'
+        }
+      ],
+      skills_data: [
+        {
+          'name': '実用技能検定準一級合格',
+          'sentence': '2022/09'
+        },
+        {
+          'name': 'TOEIC公開テスト975点',
+          'sentence': '2023/12'
+        }
+      ]
+    }
+  }
 }
 </script>
 
