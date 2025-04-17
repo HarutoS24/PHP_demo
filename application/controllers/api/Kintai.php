ea26562e81926b138ada3221cf7368d855468088
HarutoS24 <harutosato2004@gmail.com>
2025-04-17 09:17:32 +0900

    モデル処理の追加・コード修正

--- application/controllers/api/Kintai.php
+++ application/controllers/api/Kintai.php
@@ -16,23 +16,67 @@ class Kintai extends Api_Base
         return $this->_get_output($result);
     }
 
+    public function exists_clock_in($id)
+    {
+        if (!empty($id)) {
+            $result = $this->kintai_access->exists_clock_in($id);
+
+            return $this->_get_output($result);
+        }
+    }
+
+    public function exists_clock_out($id)
+    {
+        if (!empty($id)) {
+            $result = $this->kintai_access->exists_clock_out($id);
+
+            return $this->_get_output($result);
+        }
+    }
+
     public function record_clock_in($id)
     {
-        return $this->kintai_access->record_clock_in($id);
+        if (!empty($id)) {
+            return $this->kintai_access->record_clock_in($id);
+        }
     }
 
     public function record_clock_out($id)
     {
-        return $this->kintai_access->record_clock_out($id);
+        if (!empty($id)) {
+            return $this->kintai_access->record_clock_out($id);
+        }
     }
 
-    public function schedule_clock_in($id, $date, $time)
+    public function schedule_register($id, $date, $in, $out)
     {
-        return $this->kintai_access->schedule_clock_in($id, $date, $time);
+        if ($date < date('Y-m-d')) {
+            return FALSE;
+        }
+
+        if (!empty($id) and !empty($date) and !empty($in) and !empty($out)) {
+            return $this->kintai_access->schedule_clock_in($id, $date, $in) and $this->kintai_access->schedule_clock_out($id, $date, $out);
+        }
     }
 
     public function schedule_clock_out($id, $date, $time)
     {
-        return $this->kintai_access->schedule_clock_out($id, $date, $time);
+        if (!empty($id) and !empty($date) and !empty($time)) {
+            return $this->kintai_access->schedule_clock_out($id, $date, $time);
+        }
+    }
+
+    public function schedule_remove($id, $date)
+    {
+        if (!empty($id) and !empty($date)) {
+            return $this->kintai_access->schedule_remove($id, $date);
+        }
+    }
+
+    public function record_remove($id)
+    {
+        if (!empty($id)) {
+            return $this->kintai_access->record_remove($id);
+        }
     }
 }
--- application/models/Kintai_access.php
+++ application/models/Kintai_access.php
@@ -9,10 +9,50 @@ class Kintai_access extends CI_Model
         $this->load->database('kintai_db');
     }
 
-    private function get_data()
+    public function get_kintai_calendar()
     {
-        $query = $this->db->from('employee');
-        return $query->get()->result();
+        $query = $this->db
+            ->from('employee')
+            ->join('attendance_schedule', 'employee.id=attendance_schedule.employee_id', 'left')
+            ->join('attendance_records', 'employee.id=attendance_records.employee_id AND schedule_date=record_date', 'left');
+        $result = $query->get()->result();
+
+        $time_dic = [
+            'schedule_start',
+            'schedule_end',
+            'record_start',
+            'record_end',
+        ];
+
+        foreach ($result as $row) {
+            foreach ($time_dic as $name) {
+                if (! empty($row->$name)) {
+                    $row->$name = substr($row->$name, 0, 5);
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    public function exists_clock_in($id)
+    {
+        $query = $this->db->from('attendance_records')->where(
+            ['employee_id' => $id, 'record_date' => date('Y-m-d'), 'record_start!=' => NULL]
+        )->get();
+
+        return $query->num_rows() > 0;
+    }
+
+    public function exists_clock_out($id)
+    {
+        $query = $this->db->from('attendance_records')->where([
+            'employee_id' => $id,
+            'record_date' => date('Y-m-d')
+        ]);
+
+        $row = $query->get()->row();
+        return ! empty($row->record_end);
     }
 
     /**
@@ -23,7 +63,7 @@ class Kintai_access extends CI_Model
     public function record_clock_in($id)
     {
         $query = $this->db->from('attendance_records')->where(
-            ['employee_id' => $id, 'record_date' => date('Y/m/d')]
+            ['employee_id' => $id, 'record_date' => date('Y-m-d'), 'record_start!=' => NULL]
         )->get();
         //すでに同日中に出勤が押されている場合処理を受け付けない
         if ($query->num_rows() > 0) {
@@ -35,13 +75,12 @@ class Kintai_access extends CI_Model
                 'attendance_records',
                 [
                     'employee_id' => $id,
-                    'record_date' => date('Y/m/d'),
+                    'record_date' => date('Y-m-d'),
                     'record_start' => date('g:i')
                 ]
             );
 
-        //三項演算子か。。。
-        return TRUE;
+        return ($this->db->affected_rows() > 0) ? TRUE : FALSE;
     }
 
     /**
@@ -53,7 +92,7 @@ class Kintai_access extends CI_Model
     {
         $query = $this->db->from('attendance_records')->where([
             'employee_id' => $id,
-            'record_date' => date('Y/m/d')
+            'record_date' => date('Y-m-d')
         ]);
         //すでに同日中に退勤が押されている場合処理を受け付けない
         $row = $query->get()->row();
@@ -61,7 +100,8 @@ class Kintai_access extends CI_Model
             return FALSE;
         }
 
-        return $this->db->where('employee_id', $id)->set('record_end', date('g:i'))->update('attendance_records');
+        $this->db->where('employee_id', $id)->set('record_end', date('g:i'))->update('attendance_records');
+        return ($this->db->affected_rows() > 0) ? TRUE : FALSE;
     }
 
     /**
@@ -73,6 +113,10 @@ class Kintai_access extends CI_Model
      */
     public function schedule_clock_in($id, $date, $time)
     {
+        $data = [
+            'employee_id' => $id,
+            'schedule_date' => $date
+        ];
         $query = $this->db
             ->from('attendance_schedule')
             ->where([
@@ -80,14 +124,17 @@ class Kintai_access extends CI_Model
                 'schedule_date' => $date
             ]);
         //すでに指定日について出勤時間の登録がある場合update処理を行う
-        if ($query->get()->num_rows() > 0) {
-            $query->set('schedule_start', $time)->update('attendance_schedule');
-            return TRUE;
+        if ($query->get()->row()) {
+            $this->db
+                ->from('attendance_schedule')
+                ->where($data)->set('schedule_start', $time)->update('attendance_schedule');
+
+            return ($this->db->affected_rows() > 0) ? TRUE : FALSE;
         } else {
             $this->db->insert('attendance_schedule', ['employee_id' => $id, 'schedule_date' => $date, 'schedule_start' => $time]);
-        }
 
-        return $this->db;
+            return ($this->db->affected_rows() > 0) ? TRUE : FALSE;
+        }
     }
 
     /**
@@ -99,19 +146,54 @@ class Kintai_access extends CI_Model
      */
     public function schedule_clock_out($id, $date, $time)
     {
+        $data = [
+            'employee_id' => $id,
+            'schedule_date' => $date
+        ];
+        $query = $this->db
+            ->from('attendance_schedule')
+            ->where($data);
+        //すでに指定日について退勤時間の登録がある場合update処理を行う
+        if ($query->get()->num_rows() > 0) {
+            $this->db
+                ->from('attendance_schedule')
+                ->where($data)->set('schedule_end', $time)->update('attendance_schedule');
+
+            return ($this->db->affected_rows() > 0) ? TRUE : FALSE;
+        } else {
+            $this->db->insert('attendance_schedule', ['employee_id' => $id, 'schedule_date' => $date, 'schedule_end' => $time]);
+
+            return ($this->db->affected_rows() > 0) ? TRUE : FALSE;
+        }
+
+        return $this->db;
+    }
+
+    /**
+     * データベースからシフトを削除する。
+     * 今日を含めてそれ以前の日付については編集できない。
+     * @param int    $id 従業員id
+     * @param string $date 退勤予定日
+     * @return bool        操作の成否
+     */
+    public function schedule_remove($id, $date)
+    {
         $query = $this->db
             ->from('attendance_schedule')
             ->where([
                 'employee_id' => $id,
                 'schedule_date' => $date
-            ]);
-        //すでに指定日について退勤時間の登録がある場合update処理を行う
-        if ($query->get()->num_rows() > 0) {
-            $query->set('schedule_end', $time)->update('attendance_schedule');
-        } else {
-            $this->db->insert('attendance_schedule', ['employee_id' => $id, 'schedule_date' => $date, 'schedule_end' => $time]);
-        }
+            ])->delete();
 
-        return $this->db;
+        return ($this->db->affected_rows() > 0) ? TRUE : FALSE;
+    }
+
+    public function record_remove($id)
+    {
+        $this->db->where([
+            'record_date' => date('Y-m-d')
+        ])->from('attendance_records')->delete();
+
+        return ($this->db->affected_rows() > 0) ? TRUE : FALSE;
     }
 }
